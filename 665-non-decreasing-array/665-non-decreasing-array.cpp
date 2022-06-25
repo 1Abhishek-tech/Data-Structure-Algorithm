c1a1f59e775c1b956c116829ca5b91c334c75919
Abhishek Agnihotri <abhishekagni1234@gmail.com>
2022-06-25 22:11:20 +0530

    Time: 64 ms (8.12%), Space: 27.1 MB (12.14%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -2,16 +2,35 @@ class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
       // conditions to check if pos is -1 or changed from -1 once, then check pos[i+1]>=pos[i-1] or           pos[i+2]>=pos[i] and check for edge cases
-      int pos = -1;
-      for(int i=0;i<nums.size()-1;i++){
-        if(nums[i]>nums[i+1]){
-          if(pos != -1){
-            return false;
-          }
-          pos = i;
-        }
+//       int pos = -1;
+//       for(int i=0;i<nums.size()-1;i++){
+//         if(nums[i]>nums[i+1]){
+//           if(pos != -1){
+//             return false;
+//           }
+//           pos = i;
+//         }
         
+//       }
+//       return pos == -1 || pos == 0 || pos == nums.size()-2 || nums[pos+2]>=nums[pos] || nums[pos+1]>=nums[pos-1] ;
+      
+      // Another Approach
+      int changed = false;
+      
+      for(int i=0;i<nums.size()-1;i++){
+        if(nums[i]<=nums[i+1]){
+          continue;
+        }
+        if(changed)
+          return false;
+        if(i ==0 || nums[i+1]>=nums[i-1]){
+          nums[i] = nums[i+1];
+        }
+        else{
+          nums[i+1] = nums[i];
+        }
+        changed = true;
       }
-      return pos == -1 || pos == 0 || pos == nums.size()-2 || nums[pos+2]>=nums[pos] || nums[pos+1]>=nums[pos-1] ;
+      return true;
     }
 };
