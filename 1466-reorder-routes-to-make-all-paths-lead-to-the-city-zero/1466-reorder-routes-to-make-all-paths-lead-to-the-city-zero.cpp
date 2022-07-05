8c7265b925952416c92bc25775f47be347e13be1
Abhishek Agnihotri <abhishekagni1234@gmail.com>
2022-07-05 18:45:29 +0530

    Time: 1723 ms (5.02%), Space: 300.3 MB (5.02%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    unordered_map<int, unordered_set<int>> out, in;
+    int ans = 0;
+    unordered_map<int,int> vis;
+    void dfs(int node) {
+        
+        vis[node]=1;
+        for (int x: out[node]) {
+            if(vis[x])
+                continue;
+            ans++; 
+            dfs(x);
+        }
+        for (int x:in[node]) {
+			if(vis[x])
+                continue;
+			// recurse.
+            dfs(x);
+        }
+    }
+    
+    int minReorder(int n, vector<vector<int>>& connections) {
+        ans = 0;
+		// make graph.
+        for (auto x: connections) {
+            out[x[0]].insert(x[1]);
+            in[x[1]].insert(x[0]);
+        }
+		// start with node 0.
+        dfs(0);
+        return ans;
+    }
+};
