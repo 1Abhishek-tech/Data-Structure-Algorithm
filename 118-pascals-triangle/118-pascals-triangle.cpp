755aebe69eb45f99c090799c8ce121253082bbc8
Abhishek Agnihotri <abhishekagni1234@gmail.com>
2022-07-19 18:38:51 +0530

    Time: 2 ms (46.06%), Space: 6.5 MB (71.06%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
-  vector< vector<int> > tri;
     vector<vector<int>> generate(int numRows) {
-      //bottom up iterative
-      //   int n = numRows;
-      // vector< vector<int> > tri(n);
-      // for(int i = 0 ; i < n ; i++){
-      //   tri[i] = vector<int> (i+1,1);
-      //   for(int j = 1 ; j < i ; j++){
-      //     tri[i][j] = tri[i-1][j] + tri[i-1][j-1];
-      //   }
-      // }
-      // return tri;
-      
-      //top down recursive
-      if(numRows){
-        generate(numRows - 1);
-         vector<int> row(numRows,1);
-        tri.push_back(row);
-        for(int i = 1 ; i < numRows-1 ; i++){
-          tri[numRows-1][i] = tri[numRows-2][i] + tri[numRows-2][i-1];
+        int n = numRows;
+      vector< vector<int> > tri(n);
+      for(int i = 0 ; i < n ; i++){
+        tri[i] = vector<int> (i+1,1);
+        for(int j = 1 ; j < i ; j++){
+          tri[i][j] = tri[i-1][j] + tri[i-1][j-1];
         }
       }
       return tri;
