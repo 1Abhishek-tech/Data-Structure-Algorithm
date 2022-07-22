990423a4872d7eebcd5017db1dcde73598a8ce39
Abhishek Agnihotri <abhishekagni1234@gmail.com>
2022-07-22 09:48:16 +0530

    Time: 7 ms (73.71%), Space: 10.4 MB (32.53%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -11,23 +11,27 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
+      //vo sari nodes ki value jo x se choti hai vo choti list me ek k bad ek dalo(no sorting like arrangement) 
       if(head == NULL) return NULL;
-      ListNode* ptr1 = head , *currNode;
+      // ListNode* ptr1 = head , *currNode;
       ListNode* dummy11 = new ListNode() , *ptr11 = dummy11;
       ListNode* dummy12 = new ListNode() , *ptr12 = dummy12;
       
-      while(ptr1 != NULL){
-        if(ptr1->val < x ){
-          currNode = new ListNode(ptr1->val);
-          ptr11->next = currNode;
-          ptr11 = ptr11->next , ptr1 = ptr1->next;
+      while(head != NULL){
+        if(head->val < x ){
+          // currNode = new ListNode(ptr1->val);
+          // ptr11->next = currNode;
+          ptr11->next = head;
+          ptr11 = ptr11->next , head = head->next;
         }
         else {
-        currNode = new ListNode(ptr1->val);
-        ptr12->next = currNode;
-        ptr12 = ptr12->next , ptr1 = ptr1->next;
+        // currNode = new ListNode(ptr1->val);
+        // ptr12->next = currNode;
+        ptr12->next = head;
+        ptr12 = ptr12->next , head = head->next;
         }
       }
+      ptr12->next = NULL;
       ptr11->next = dummy12->next;
       return dummy11->next;
       
