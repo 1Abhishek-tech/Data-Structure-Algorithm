21f0d4201131675d8fce076bc2fbab3ad105364a
Abhishek Agnihotri <abhishekagni1234@gmail.com>
2022-08-16 10:53:37 +0530

    Time: 30 ms (84.48%), Space: 10.7 MB (71.24%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char,pair<int,int>> map;
+//         unordered_map<char,pair<int,int>> map;
       
+//       for(int i = 0 ; i < s.length() ; i++){
+//         map[s[i]].first++; map[s[i]].second = i ;
+//       }
+//       int idx = -1;
+//       for(int i = 0 ; i < s.length() ; i++){
+//         if(map[s[i]].first == 1){ idx = map[s[i]].second; break; }
+//       }
+//       return idx;
+      pair<int,int> map[26];
       for(int i = 0 ; i < s.length() ; i++){
-        map[s[i]].first++; map[s[i]].second = i ;
+        int pos = s[i] - 'a';
+        map[pos].first++; map[pos].second = i;
       }
-      int idx = -1;
-      for(int i = 0 ; i < s.length() ; i++){
-        if(map[s[i]].first == 1){ idx = map[s[i]].second; break; }
+      int idx = INT_MAX;
+      for(int i = 0 ; i < 26 ; i++){
+        if(map[i].first == 1){
+          idx = min(idx,map[i].second);
+        }
       }
-      return idx;
+      return idx == INT_MAX ? -1 : idx;
     }
 };
