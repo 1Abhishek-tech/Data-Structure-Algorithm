f4ecf8ec93ee194bd824ce9405d08375ab0798be
Abhishek Agnihotri <abhishekagni1234@gmail.com>
2022-09-02 11:34:08 +0530

    Time: 21 ms (10.46%), Space: 11.7 MB (54.77%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -12,22 +12,39 @@ class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
         if(head == NULL || head->next == NULL || k == 0) return head;
-      ListNode* temp = head , *end;
+      
+      ListNode* temp = head;
       int length = 1;
-      stack<ListNode*> st;
       while(temp->next != NULL){
-        length++; st.push(temp); temp = temp->next; 
+        length++; temp = temp->next; 
       }
+      temp->next = head;
       k = k%length;
-      for(int i = 1 ; i <= k ; i++){
-        // temp = head;
-        // while(temp->next->next != NULL) temp = temp->next;
-        temp = st.top(); st.pop();
-        end = temp->next;
-        temp->next = NULL;
-        end->next = head;
-        head = end;
+      int steps = length - k;
+      temp = head;
+      for(int i = 1 ; i < steps ; i++){
+        temp = temp->next;
       }
+      head = temp->next;
+      temp->next = NULL;
       return head;
+      // 2 approaches discuss below 1 with On^2 and other using stack with On TC and On SC
+      // ListNode* temp = head , *end;
+      // int length = 1;
+      // stack<ListNode*> st;
+      // while(temp->next != NULL){
+      //   length++; st.push(temp); temp = temp->next; 
+      // }
+      // k = k%length;
+      // for(int i = 1 ; i <= k ; i++){
+      //   // temp = head;
+      //   // while(temp->next->next != NULL) temp = temp->next;
+      //   temp = st.top(); st.pop();
+      //   end = temp->next;
+      //   temp->next = NULL;
+      //   end->next = head;
+      //   head = end;
+      // }
+      // return head;
     }
 };
