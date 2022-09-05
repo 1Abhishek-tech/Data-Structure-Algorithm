2ea25b688ec5437c5f986da05046b7c39b6727e7
Abhishek Agnihotri <abhishekagni1234@gmail.com>
2022-09-05 08:56:29 +0530

    Time: 62 ms (5.44%), Space: 11.7 MB (86.69%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -19,27 +19,41 @@ public:
 */
 
 class Solution {
+  private:
+  void bfs(Node* root , int level , vector<vector<int>> &res){
+    if(root == NULL) return ;
+    if(res.size() == level) res.push_back({});
+    
+    res[level].push_back(root->val);
+    for(auto children : root->children){
+      bfs(children , level+1 , res);
+    }
+  }
 public:
     vector<vector<int>> levelOrder(Node* root) {
-        vector<vector<int>> ans;
-      if(root == NULL) return ans;
-      queue<Node*> q; q.push(root);
+      vector<vector<int> > res;
+      bfs(root , 0 , res);
+      return res;
+      //BFS Approach
+//         vector<vector<int>> ans;
+//       if(root == NULL) return ans;
+//       queue<Node*> q; q.push(root);
       
-      while(!q.empty()){
-        int size = q.size();
-        vector<int> temp ;
-        while(size--){
-          Node* cur = q.front(); q.pop();
-          temp.push_back(cur->val);
-          for(auto nxt : cur->children){
-            if(nxt != NULL){
-              q.push(nxt);
-            }
-          }
+//       while(!q.empty()){
+//         int size = q.size();
+//         vector<int> temp ;
+//         while(size--){
+//           Node* cur = q.front(); q.pop();
+//           temp.push_back(cur->val);
+//           for(auto nxt : cur->children){
+//             if(nxt != NULL){
+//               q.push(nxt);
+//             }
+//           }
           
-        }
-        ans.push_back(temp);
-      }
-      return ans;
+//         }
+//         ans.push_back(temp);
+//       }
+//       return ans;
     }
 };
