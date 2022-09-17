ad519cbe425903bd022076e3c739d07f6837ca71
Abhishek Agnihotri <abhishekagni1234@gmail.com>
2022-09-17 18:46:55 +0530

    Time: 765 ms (10.35%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-select Email from Person group by email having count(*) > 1;
+# select Email from Person group by email having count(*) > 1;
+
+select distinct p1.email as Email from Person p1 inner join Person p2 on p1.email = p2.email and p1.id != p2.id;
