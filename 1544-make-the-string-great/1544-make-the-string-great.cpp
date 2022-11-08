07f357ae2dd9700cb4a317d6335e055ab0768f22
Abhishek Agnihotri <abhishekagni1234@gmail.com>
2022-11-08 23:26:57 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (26.42%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,27 +1,49 @@
 class Solution {
 public:
     string makeGood(string s) {
-        string ans;
-         int flag=1;
-        while(flag){
-            string str;
-            int prev=s.size();
-            int i=0;
-        for(i=0;i<s.size()-1;i++){
-            if(s[i]-'a'==s[i+1]-'A'||s[i]-'A'==s[i+1]-'a')i++;
-            else str.push_back(s[i]);
+        int n = s.length();
+        if(n == 1) return s;
+        
+        stack<char> st;
+        st.push(s[0]);
+        
+        for(int i = 1 ; i < n ; i++){
+            if(!st.empty() && abs(st.top() - s[i]) == 32){
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
         }
-        if(i<s.size()){
-        str.push_back(s[i]);
+        
+        string ans = "";
+        while(!st.empty()){
+            ans += st.top(); st.pop();
         }
-        if(str.size()==prev||str.size()==0){
-            ans=str;
-            flag=0;
-        }
-        else{
-            s=str;
-        }
-        }
+        
+        reverse(ans.begin() , ans.end());
         return ans;
+        // string ans;
+        //  int flag=1;
+        // while(flag){
+        //     string str;
+        //     int prev=s.size();
+        //     int i=0;
+        // for(i=0;i<s.size()-1;i++){
+        //     if(s[i]-'a'==s[i+1]-'A'||s[i]-'A'==s[i+1]-'a')i++; //if (abs(currChar - nextChar) == 32) 
+        //     else str.push_back(s[i]);
+        // }
+        // if(i<s.size()){
+        // str.push_back(s[i]);
+        // }
+        // if(str.size()==prev||str.size()==0){
+        //     ans=str;
+        //     flag=0;
+        // }
+        // else{
+        //     s=str;
+        // }
+        // }
+        // return ans;
     }
 };
